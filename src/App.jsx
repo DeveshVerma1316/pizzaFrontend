2087b89dacc85161c47894745b05c5dc43838e0d
devesh verma <deveshsingh9756@gmail.com>
2025-04-02 13:30:14 +0530

    Added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Layout from './Layouts/Layout'
 import Home from './Pages/Home'
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
  
   return (
     <div >
-      <Home/>
+      <Layout>
+        <Home />
+        
+      </Layout>
       </div>
   
   )
