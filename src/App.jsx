719cc4c1ecf9d4e952d428a90c6b516f5750c9be
devesh verma <deveshsingh9756@gmail.com>
2025-04-02 14:20:10 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Home from './Pages/Home'
 import SignUp from './Pages/Auth/Signup'
+import Login from './Pages/Auth/Login'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path = '/auth/signup' element={<SignUp />} />
+      <Route path = '/auth/login' element={<Login />} />
     </Routes>
     
     </>
