5764e0d078f542ececd65286ccc082501a41a0c2
devesh verma <deveshsingh9756@gmail.com>
2025-04-30 10:06:48 +0530

    Added presenter container design pattern

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import { Toaster } from 'react-hot-toast'
 ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster/>
   </BrowserRouter>,
 )
