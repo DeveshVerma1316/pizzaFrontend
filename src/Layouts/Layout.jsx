0cbe4d7d6d71ad1665804b61d69f818499765042
devesh verma <deveshsingh9756@gmail.com>
2025-05-02 18:23:15 +0530

    Added redux thunks for logout and login

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,48 +1,71 @@
-import Pizzalogo from "../assets/Images/pizza1.png"
-import Footer from './../Components/Footer';
-function Layout({children}) {
-    return(
-        <div>
-
-            <nav className="flex items-cneter justify-around h-16 text-[#687280] font-mono border-none shadow-md">
-
-                <div className="flex items-center justify-center ">
-                    <p>Pizza App</p>
-                    <img src={Pizzalogo} alt = "Pizza logo"/>
-                </div>
-                <div className="hidden md:block">
-                    <ul className="flex gap-4">
-                        <li className = 'hover:text-[#FF9110]'>
+import { useDispatch, useSelector } from 'react-redux';
+ import Footer from '../Components/Footer';
+ import Pizzalogo from '../assets/Images/pizza1.png';
+ import { Link } from 'react-router-dom';
+ import { logout } from '../Redux/Slices/AuthSlice';
+ 
+ // eslint-disable-next-line react/prop-types
+ function Layout({ children }) {
+ 
+     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+     const dispatch = useDispatch();
+ 
+     async function handleLogout(e) {
+         e.preventDefault();
+         dispatch(logout());
+         
+     }
+ 
+     return (
+         <div>
+ 
+             <nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
+ 
+                 <div className="flex items-center justify-center">
+                     <p>Pizza App</p>
+                     <img src={Pizzalogo} alt="Pizza logo" />
+                 </div>
+ 
+                 <div className='hidden md:block'>
+                     <ul className='flex gap-4'>
+ 
+                         <li className='hover:text-[#FF9110]'>
                              { ' ' }
-                             <p>Menu{ ' ' }</p>
-                        </li>
-
-                        <li className = 'hover:text-[#FF9110]'>
+                             <p>Menu {' '}</p>
+                         </li>
+ 
+                         <li className='hover:text-[#FF9110]'>
                              { ' ' }
-                             <p>Services{ ' ' }</p>
-                        </li>
-
-                        <li className = 'hover:text-[#FF9110]'>
+                             <p>Services {' '}</p>
+                         </li>
+ 
+                         <li className='hover:text-[#FF9110]'>
                              { ' ' }
-                             <p>About{ ' ' }</p>
-                        </li>
-
-                      
-                        
-
-                    </ul>
-
-                </div>
-
-
-            </nav>
-
-            {children}
-
-
-            <Footer />
-        </div>
-
-    )
-}
+                             <p>About {' '}</p>
+                         </li>
+ 
+                     </ul>
+                 </div>
+ 
+                 <div>
+                     <ul className='flex gap-4'>
+                         <li className='hover:text-[#FF9110]'>
+                             {isLoggedIn ? (
+                                 <Link onClick={handleLogout}>Logout</Link>
+                             ) : (
+                                 <Link to={'/auth/login'}>Login</Link>
+                             )}
+                         </li>
+                     </ul>
+                 </div>
+ 
+             </nav>
+ 
+                 {children}
+ 
+             <Footer />
+         </div>  
+     )
+ }
+ 
 export default Layout;
