4e73dc04f3bb9ad83ca3c3745ecce1bc9948e7dc
devesh verma <deveshsingh9756@gmail.com>
2025-05-02 19:00:43 +0530

    Added product slice

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux';
  import Footer from '../Components/Footer';
  import Pizzalogo from '../assets/Images/pizza1.png';
- import { Link } from 'react-router-dom';
+ 
+ import { Link, useNavigate } from 'react-router-dom';
  import { logout } from '../Redux/Slices/AuthSlice';
  
  // eslint-disable-next-line react/prop-types
@@ -9,11 +10,12 @@ import { useDispatch, useSelector } from 'react-redux';
  
      const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
      const dispatch = useDispatch();
+     const navigate = useNavigate();
  
      async function handleLogout(e) {
          e.preventDefault();
          dispatch(logout());
-         
+ 
      }
  
      return (
@@ -21,7 +23,10 @@ import { useDispatch, useSelector } from 'react-redux';
  
              <nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
  
-                 <div className="flex items-center justify-center">
+                
+                 <div className="flex items-center justify-center"
+                     onClick={() => navigate('/')}
+                 >
                      <p>Pizza App</p>
                      <img src={Pizzalogo} alt="Pizza logo" />
                  </div>
